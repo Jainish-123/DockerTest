d5972f76d3ee5b46c563df4920741bcb020ff702
Jainishkumar Bhaveshkumar Patel <jn891368@dal.ca>
2024-08-03 23:24:19 -0300

    route change

--- deployed-backend-1/app/src/routes/deployBackendRoute.js
+++ deployed-backend-1/app/src/routes/deployBackendRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { deployBackend } = require("../controllers/deployBackendController");
 
 const router = express.Router();
-router.post("/deploy-backend", deployBackend);
+router.post("/", deployBackend);
 
 module.exports = router;
